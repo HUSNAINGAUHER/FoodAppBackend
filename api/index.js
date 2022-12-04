08105447e02b1c52a2b7cdf0e5f4f7e6fa90bd38
Husnain Gauher <husnaingauher@Husnains-MacBook-Pro.local>
2022-12-04 19:09:34 +0500

    Refs

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.set('trust proxy', 1);
 
 app.use(express.json({ limit: '4mb' }));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "https://food-qw63nsy52-husnaingauher.vercel.app" }));
 
 //root route
 app.get('/', (req, res) => {
